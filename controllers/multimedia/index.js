5e5c7280cef17c56fc010e7894c46561db566f91
Alex Aguilar <auza920205@gmail.com>
2018-08-25 01:29:56 -0500

    update file finished

--- controllers/multimedia/index.js
+++ controllers/multimedia/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Multimedia = require('../../models/multimedia.js');
+var Usuario = require('../../models/usuario.js');
 var FileStorage = require('../../utils/fileStorage.js');
 
 module.exports = function(router){
@@ -21,12 +22,30 @@ module.exports = function(router){
 
   router.post('/', function (req, res) {
     var response;
-    console.log(req.body.id);
     var fileStorage = new FileStorage(req);
     fileStorage.store()
     .then(file_name => {
       var multimedia = new Multimedia(req.body.nombre_multimedia, file_name);
       multimedia.create(req.body.id)
+      .then(data => {
+        if(data)
+          res.send(response = {state: true, data: data});
+        res.send(response = {state: false, data: data});
+      });
+    });
+  });
+
+  router.put('/', function(req, res) {
+    var response;
+    var fileStorage = new FileStorage(req);
+    fileStorage.store()
+    .then(file_name => {
+      req.body.usuario.multimedia.map(file => {
+        if(file._id === req.body.updatingFileID)
+          file.nombre_archivo = file_name;
+      });
+      var usuario = new Usuario();
+      usuario.update(req.body.usuario)
       .then(data => {
         if(data)
           res.send(response = {state: true, data: data});
